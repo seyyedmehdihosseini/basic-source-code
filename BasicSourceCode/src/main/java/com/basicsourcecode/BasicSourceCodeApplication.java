2a235ae5a302b48acf22cb8df6b4c6dc6d05bf4d
mahdi Hosseini <ma.hosseini@Behsacorp.com>
2024-09-11 15:13:40 +0330

    create project

---
+++ BasicSourceCode/src/main/java/com/basicsourcecode/BasicSourceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.basicsourcecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSourceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSourceCodeApplication.class, args);
+    }
+
+}
---
+++ BasicSourceCode/src/test/java/com/basicsourcecode/BasicSourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicsourcecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSourceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

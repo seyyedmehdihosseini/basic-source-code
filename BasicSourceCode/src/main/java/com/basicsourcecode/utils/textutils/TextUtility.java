edf833b2dde3f346677138edf297853f9b11f7de
mehdi_hosseini <mehdi1997hosseini@gmail.com>
2024-12-01 12:39:48 +0330

    change to TextUtility.java

--- BasicSourceCode/src/main/java/com/basicsourcecode/utils/textutils/TextUtility.java
+++ BasicSourceCode/src/main/java/com/basicsourcecode/utils/textutils/TextUtility.java
@@ -4,8 +4,9 @@ public final class TextUtility {
 
     /**
      * @param text The text for convert to capitalize.
-     * @EN Every word should start with a capital letter.
-     * @FA هر کلمه با حرف بزرگ شروع شود.
+     * @EN : The first letter of each word should be uppercase and the rest should be lowercase.
+     * @FA : حرف اول هر کلمه بزرگ و بقیه حروف کوچک باشند
+     * @example : "salamManMehdi hosseini hastam" to "SalamManMehdi Hosseini Hastam"
      */
     public static String toCapitalize(String text) {
         if (text == null || text.isEmpty()) return text;
@@ -22,7 +23,7 @@ public final class TextUtility {
     /**
      * @param text The text for convert to camel case.
      * @EN Connect the words and start each new word with a capital letter.
-     * @FA کلمات به هم متصل شوند و هر کلمه جدید با حرف بزرگ شروع شود
+     * @FA : کلمات به هم متصل شوند و هر کلمه جدید با حرف بزرگ شروع شود فقط اولین کلمه تماما با حروف کوچک است.
      */
     public static String toCamelCase(String text) {
         if (text == null || text.isEmpty()) {
@@ -43,41 +44,33 @@ public final class TextUtility {
         return camelCaseString.toString();
     }
 
-    /**
-     * @param text The text for convert to camel case.
-     * @EN Connect the words and start each new word with a capital letter
-     * @FA کلمات به هم متصل شوند و هر کلمه جدید با حرف بزرگ شروع شود
-     */
-    public static String titleToCamelCase(String text) {
-        if (text == null || text.isEmpty()) return text;
-        String[] words = splitWords(text);
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            result.append(i == 0 ? word.toLowerCase() : capitalizeWord(word));
-        }
-
-        return result.toString();
-    }
+//    public static String toSnakeCase(String text) {
+//        if (text == null || text.isEmpty()) return text;
+//        String[] words = splitWords(text);
+//        return String.join("_", words).toLowerCase();
+//    }
 
     /**
-     * @param text The name entity class or name model.
+     * @param nameEntityClass The name entity class or name model.
      * @EN Words are separated by "_" and all letters are lowercase.
-     * @FA کلمات با "_" جدا شوند و همه حروف کوچک شوند.
+     * @FA : کلمات با "_" جدا شوند و همه حروف کوچک شوند.
+     * @use : for name table
+     * @example : "salamManMehdiHosseiniHastam" to "salam_Man_Mehdi_Hosseini_Hastam"
      */
-    public static String toSnakeCase(String text) {
-        if (text == null || text.isEmpty()) return text;
-        String[] words = splitWords(text);
-        return String.join("_", words).toLowerCase();
+    public static String toSnakeCase(String nameEntityClass) {
+        return nameEntityClass.replaceAll("([A-Z])([A-Z][a-z])", "$1_$2")
+                .replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
     }
 
-    /*
-    protected static String toSnakeCase(String nameEntityClass) {
-        return nameEntityClass.replaceAll("([A-Z])([A-Z][a-z])", "$1_$2")
-                .replaceAll("([a-z])([A-Z])", "$1_$2");
+    /**
+     * @param word The text entered to method.
+     * @EN An auxiliary method for capitalizing a word: the first letter is capitalized, the rest are small.
+     * @FA : متد کمکی برای Capitalizing یک کلمه: حرف اول بزرگ، باقی کوچک.
+     */
+    private static String capitalizeWord(String word) {
+        if (word == null || word.isEmpty()) return word;
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
-*/
 
     /**
      * @param text The text entered method
@@ -89,13 +82,51 @@ public final class TextUtility {
     }
 
     /**
-     * @param text The text entered to method.
-     * @EN An auxiliary method for capitalizing a word: the first letter is capitalized, the rest are small.
-     * @FA متد کمکی برای Capitalizing یک کلمه: حرف اول بزرگ، باقی کوچک.
+     * EN : Removing a text from the beginning of a string and returning the continuation of that string
+     * FA : حذف یک متن از ابتدا یک رشته و برگرداندن ادامه آن رشته
+     *
+     * @example : "basicSourceCode\src\main\java\com\basicSourceCode" with "src\main\java" to "com\basicSourceCode"
      */
-    private static String capitalizeWord(String text) {
-        if (text == null || text.isEmpty()) return "";
-        return Character.toUpperCase(text.charAt(0)) + text.substring(1).toLowerCase();
+    public static String removeBeforeAndIncludingKeyword(String input, String keyword) {
+        int index = input.indexOf(keyword); // پیدا کردن مکان کلمه کلیدی
+        if (index != -1) {
+            // محاسبه موقعیت پایان کلمه کلیدی
+            int endOfKeyword = index + keyword.length();
+            return input.substring(endOfKeyword).trim(); // بازگرداندن رشته از بعد کلمه کلیدی
+        }
+        return input; // اگر کلمه کلیدی وجود ندارد، رشته اصلی را برگردان
+    }
+
+    /**
+     * EN : Remove before a word or a text and return the continuation of that text
+     * FA : حذف قبل از یک کلمه یا یک متن و برگرداندن ادامه آن متن
+     *
+     * @example : "basicSourceCode\src\main\java\com\basicSourceCode" with "src\" to "src\main\java\com\basicSourceCode"
+     */
+    public static String removeTextBeforeKeyword(String input, String keyword) {
+        int index = input.indexOf(keyword); // پیدا کردن مکان کلمه کلیدی
+        if (index != -1) {
+            // محاسبه موقعیت پایان کلمه کلیدی
+            return input.substring(index).trim(); // بازگرداندن رشته از بعد کلمه کلیدی
+        }
+        return input; // اگر کلمه کلیدی وجود ندارد، رشته اصلی را برگردان
+    }
+
+
+    /**
+     * @param camelCaseText entered camel case text
+     *                      EN : Conversion to CamelCase with the difference that a space is created between uppercase and lowercase letters
+     *                      FA : تبدیل به CamelCase با این تفاوت که بین حروف کوچک و بزرگ یک فاصله ایجاد میشود
+     * @example : "salamManMehdiHosseiniHastam" to "salam Man Mehdi Hosseini Hastam"
+     */
+    public static String toSplitCamelCase(String camelCaseText) {
+        if (camelCaseText == null || camelCaseText.isEmpty()) {
+            return camelCaseText;
+        }
+
+        // استفاده از regex برای جدا کردن حروف بزرگ
+        return camelCaseText.replaceAll("([a-z])([A-Z])", "$1 $2")
+                .replaceAll("([A-Z])([A-Z][a-z])", "$1 $2");
     }
 
 }
